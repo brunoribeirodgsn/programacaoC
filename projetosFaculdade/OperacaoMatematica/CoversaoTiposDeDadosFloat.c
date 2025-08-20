535e598306a485152c3009307b2adf22762ac5ad
bruno ribeiro <101024960+brunoribeirodgsn@users.noreply.github.com>
2025-08-20 02:09:28 +0000

    primeiro commit

---
+++ projetosFaculdade/OperacaoMatematica/CoversaoTiposDeDadosFloat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    //FLOAT
+    float x = 5.5;
+    float y = 2.2;
+    float soma = x + y;
+    float diferenca = x - y;
+    float produto = x * y;
+    float quociente = x / y; // Divisão de ponto fluante
+
+    printf("");
+
+    printf("\n\n");
+
+
+
+}
