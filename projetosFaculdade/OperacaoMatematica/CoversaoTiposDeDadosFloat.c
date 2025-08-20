6d16e283b4d146cce45366149b99dfad2fa822fc
bruno ribeiro <101024960+brunoribeirodgsn@users.noreply.github.com>
2025-08-20 23:14:20 +0000

    primeiro commit

--- projetosFaculdade/OperacaoMatematica/CoversaoTiposDeDadosFloat.c
+++ projetosFaculdade/OperacaoMatematica/CoversaoTiposDeDadosFloat.c
@@ -10,10 +10,32 @@ int main(){
     float produto = x * y;
     float quociente = x / y; // Divisão de ponto fluante
 
-    printf("");
+    printf("Soma: %.2f\n", soma);
+    printf("Diferença: %.2f\n", diferenca);
+    printf("Produto: %.2f\n", produto);
+    printf("Quociente: %.2f\n", quociente);
 
     printf("\n\n");
 
+    //FLOAT 2
 
+    int a = 10;
+    float b = 3.5;
+    float resultado = a + b; // 'a' e convertido implicitamente para float
+
+    printf("Resultado: %.2f\n", resultado);
+
+    printf("\n\n");
+    
+    //FLOAT 3
+    int c = 10;
+    int d = 3;
+    float quociente = (float) c / d; // 'a' é explicitamente convertido para float
+ 
+    printf("Quociente: %.2f\n", quociente);
+
+
+    printf("\n\n");
+    return 0;
 
 }
